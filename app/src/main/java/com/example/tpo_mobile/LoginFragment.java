b5f4581d14217c2fd79c13acf3c168cae69b9b27
Daniela <dggtn97@gmail.com>
2025-09-06 12:15:18 -0300

    main activity va al fragment de login atraves de navgraph

--- app/src/main/java/com/example/tpo_mobile/LoginFragment.java
+++ app/src/main/java/com/example/tpo_mobile/LoginFragment.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class SignUpFragment extends Fragment {
+public class LoginFragment extends Fragment {
     @Nullable
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
