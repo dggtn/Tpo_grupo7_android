95bc3344328aa75257d0cba3ac06f483a6380ae1
Daniela <dggtn97@gmail.com>
2025-09-06 11:28:27 -0300

    dejo solo el main activity (pantalla de login ) como activity para el resto hacer de tipo fragment

--- app/src/main/java/com/example/tpo_mobile/MainActivity.java
+++ app/src/main/java/com/example/tpo_mobile/MainActivity.java
@@ -1,93 +1,76 @@
 package com.example.tpo_mobile;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.tpo_mobile.services.AppService;
 
 public class MainActivity extends AppCompatActivity {
-        private static final String TAG = "ActivityLifecycle";
-
-        private Button login;
-        private Button signUp;
-
-        AppService appService;
-
+    private static final String TAG = "ActivityLifecycle";
+    AppService appService;
     private ArrayAdapter<String> adapter;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            Log.d(TAG, "⭐ onCreate: La Activity está siendo creada");
-            setContentView(R.layout.activity_main);
-            login =  findViewById(R.id.home_login);
-            login.setOnClickListener((View view)->{
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-            });
-            signUp =  findViewById(R.id.home_signUp);
-            signUp.setOnClickListener((View view)->{
-                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
-                startActivity(intent);
-            });
-        };
 
 
 
     @Override
-            protected void onStart() {
-                super.onStart();
-                Log.d(TAG, "⭐ onStart: La Activity está a punto de hacerse visible");
-            }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "⭐ onCreate: La Activity está siendo creada");
+        setContentView(R.layout.main_activity);
 
-            @Override
-            protected void onResume() {
-                super.onResume();
-                Log.d(TAG, "⭐ onResume: La Activity es visible y tiene el foco");
-            }
+    };
 
-            @Override
-            protected void onPause() {
-                super.onPause();
-                Log.d(TAG, "⭐ onPause: La Activity está perdiendo el foco");
-            }
-
-            @Override
-            protected void onStop() {
-                super.onStop();
-                Log.d(TAG, "⭐ onStop: La Activity ya no es visible");
-            }
-
-            @Override
-            protected void onRestart() {
-                super.onRestart();
-                Log.d(TAG, "⭐ onRestart: La Activity está volviendo a empezar después de detenerse");
-            }
-
-            @Override
-            protected void onDestroy() {
-                super.onDestroy();
-                Log.d(TAG, "⭐ onDestroy: La Activity está siendo destruida");
-            }
-
-
-
-            @Override
-            protected void onSaveInstanceState(Bundle outState) {
-                super.onSaveInstanceState(outState);
-                Log.d(TAG, "⭐ onSaveInstanceState: Guardando el estado de la Activity");
-            }
-
-            @Override
-            protected void onRestoreInstanceState(Bundle savedInstanceState) {
-                super.onRestoreInstanceState(savedInstanceState);
-                Log.d(TAG, "⭐ onRestoreInstanceState: Restaurando el estado guardado de la Activity");
-            }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "⭐ onStart: La Activity está a punto de hacerse visible");
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "⭐ onResume: La Activity es visible y tiene el foco");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "⭐ onPause: La Activity está perdiendo el foco");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "⭐ onStop: La Activity ya no es visible");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "⭐ onRestart: La Activity está volviendo a empezar después de detenerse");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "⭐ onDestroy: La Activity está siendo destruida");
+    }
+
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "⭐ onSaveInstanceState: Guardando el estado de la Activity");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG, "⭐ onRestoreInstanceState: Restaurando el estado guardado de la Activity");
+    }
+}
---
+++ app/src/main/java/com/example/tpo_mobile/SignUpFragment.java
@@ -0,0 +1,16 @@
+package com.example.tpo_mobile;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SignUpFragment extends Fragment {
+    @Nullable
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
