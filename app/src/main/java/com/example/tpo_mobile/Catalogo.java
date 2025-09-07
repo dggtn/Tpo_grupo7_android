1f0db29a531f4534bc2d186b8cd266359d8e897c
Daniela <dggtn97@gmail.com>
2025-09-07 09:36:29 -0300

    creo ejemplo catalogo ,agrego navegacion a navgraph y creo fragment curso para hacer listado de cursos

--- app/src/main/java/com/example/tpo_mobile/Catalogo.java
+++ app/src/main/java/com/example/tpo_mobile/Catalogo.java
@@ -24,7 +24,7 @@ public class Catalogo extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragmentcatalogo_clases, container, false);
+        return inflater.inflate(R.layout.fragment_curso, container, false);
     }
 
     @Override
