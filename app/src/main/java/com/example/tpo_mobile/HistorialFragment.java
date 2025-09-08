e5d6e39460ee8f2ebfe12a0c179cbbca739dfa26
Daniela <dggtn97@gmail.com>
2025-09-08 14:29:12 -0300

    agrego navegacion del bottomvar para que vaya a historia

---
+++ app/src/main/java/com/example/tpo_mobile/HistorialFragment.java
@@ -0,0 +1,43 @@
+package com.example.tpo_mobile;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class HistorialFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        Log.d("historial fragment", "On create login fragment");
+
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_historial, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d("login fragment", "On start login fragment");
+    }
+}
